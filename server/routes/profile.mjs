ca7d663c0906edf15201c2356f604a5d9c6aa487
Dataquark <daniil.razlivanov99@gmail.com>
2025-04-20 14:18:37 +0300

    minor adjustments

--- server/routes/profile.mjs
+++ server/routes/profile.mjs
@@ -4,10 +4,6 @@ import { authMiddleware } from '../utils/middlewares.mjs';
 import upload from '../utils/upload.mjs';
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const router = Router();
 

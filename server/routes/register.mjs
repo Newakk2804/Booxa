e530d34320f97601a6a6ac34e5647767ace7aa14
Dataquark <daniil.razlivanov99@gmail.com>
2025-04-22 15:02:15 +0300

    add validation register user

--- server/routes/register.mjs
+++ server/routes/register.mjs
@@ -1,27 +1,46 @@
 import { Router } from 'express';
 import User from '../models/Users.mjs';
 import { hashPassword } from '../utils/helpers.mjs';
+import { checkSchema, validationResult } from 'express-validator';
+import { registerUserValidationSchema } from '../utils/validationSchema.mjs';
 
 const router = Router();
 
 router.get('/register', (req, res) => {
   const locals = {
     title: 'Регистрация',
+    errors: {},
+    oldInput: {},
   };
   res.render('register', locals);
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', checkSchema(registerUserValidationSchema), async (req, res) => {
+  const errors = validationResult(req);
   const { mail, password, secondPassword } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('register', { errors: errors.mapped(), oldInput: req.body });
+  }
 
   const CheckUser = await User.findOne({ mail });
 
-  if (password !== secondPassword) throw new Error('passwords do not match');
+  if (CheckUser) {
+    return res.status(422).render('register', {
+      errors: { mail: { msg: 'Такой пользователь уже существует' } },
+      oldInput: req.body,
+    });
+  }
 
   const hashedPassword = hashPassword(password);
-  const newUser = new User({ mail, password: hashedPassword, role: 'user', imageUrl: 'img/user.jpg'});
+  const newUser = new User({
+    mail,
+    password: hashedPassword,
+    role: 'user',
+    imageUrl: 'img/user.jpg',
+  });
   try {
-    const savedUser = await newUser.save();
+    await newUser.save();
     return res.redirect('login');
   } catch (error) {
     return res.status(400).send({ msg: error });
--- server/utils/validationSchema.mjs
+++ server/utils/validationSchema.mjs
@@ -10,3 +10,34 @@ export const loginUserValidationSchema = {
     },
   },
 };
+
+export const registerUserValidationSchema = {
+  mail: {
+    isEmail: {
+      errorMessage: 'Введите корректный email',
+    },
+    notEmpty: {
+      errorMessage: 'Поле почты не должно быть пустым',
+    },
+  },
+  password: {
+    isLength: {
+      options: { min: 6 },
+      errorMessage: 'Пароль должен содержать минимум 6 символов',
+    },
+    notEmpty: {
+      errorMessage: 'Введите пароль',
+    },
+  },
+  secondPassword: {
+    custom: {
+      options: (value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Пароли не совпадают');
+        }
+        return true;
+      },
+    },
+  },
+};
+
